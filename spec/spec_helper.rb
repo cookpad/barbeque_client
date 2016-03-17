759403fe8d0a1f806a452282d56bafb314513ec2
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-03-17 18:14:18 +0900

    Prepare to write spec for rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'barbeque'
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('./dummy/config/environment', __dir__)
+require 'rspec/rails'
