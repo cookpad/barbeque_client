1beffa447bc637bb4c47a5cadfd633eee717c1e7
Okumura Takahiro <hfm@cookpad.com>
2025-01-16 11:34:07 +0900

    Update BarbequeAdapter for Rails 7.2 compatibility (#18)
    * Update BarbequeAdapter for Rails 7.2 compatibility
    
    In Rails 7.2, a new class called ActiveJob::QueueAdapter::AbstractAdapter was added. It looks like custom adapters should basically inherit from this class.
    Considering the support period for Rails, it would be better to stop supporting versions less than Rails 7.2 and make codes simpler.
    
    - Update gemspec to require Rails 7.2 (ActiveJob) and above
    - Refactor BarbequeAdapter to inherit from ActiveJob::AbstractAdapter
    
    * Rails 7.2 requires Ruby 3.1 or higher

--- lib/active_job/queue_adapters/barbeque_adapter.rb
+++ lib/active_job/queue_adapters/barbeque_adapter.rb
@@ -1,36 +1,24 @@
 module ActiveJob
   module QueueAdapters
-    class BarbequeAdapter
-      # Interface for ActiveJob 5.0
+    class BarbequeAdapter < ActiveJob::QueueAdapters::AbstractAdapter
       def enqueue(job)
-        BarbequeAdapter.enqueue(job)
+        execution = BarbequeClient.enqueue(
+          job:     job.class.to_s,
+          message: ActiveJob::Arguments.serialize(job.arguments),
+          queue:   job.queue_name,
+        )
+        job.job_id = execution.message_id
       end
 
       def enqueue_at(job, timestamp)
-        BarbequeAdapter.enqueue_at(job, timestamp)
-      end
-
-      class << self
-        # Interface for ActiveJob 4.2
-        def enqueue(job)
-          execution = BarbequeClient.enqueue(
-            job:     job.class.to_s,
-            message: ActiveJob::Arguments.serialize(job.arguments),
-            queue:   job.queue_name,
-          )
-          job.job_id = execution.message_id
-        end
-
-        def enqueue_at(job, timestamp)
-          delay_seconds = (timestamp - Time.now.to_f).round
-          execution = BarbequeClient.enqueue(
-            job:     job.class.to_s,
-            message: ActiveJob::Arguments.serialize(job.arguments),
-            queue:   job.queue_name,
-            delay_seconds: delay_seconds,
-          )
-          job.job_id = execution.message_id
-        end
+        delay_seconds = (timestamp - Time.now.to_f).round
+        execution = BarbequeClient.enqueue(
+          job:     job.class.to_s,
+          message: ActiveJob::Arguments.serialize(job.arguments),
+          queue:   job.queue_name,
+          delay_seconds: delay_seconds,
+        )
+        job.job_id = execution.message_id
       end
     end
   end
